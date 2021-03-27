dba6c57f3ad30e4c84ddd16dc950d1aeb94d706f
Nicholas <52936504+z-dot@users.noreply.github.com>
2021-03-27 12:54:08 +0000

    Fixes and improvements

--- parser.py
+++ parser.py
@@ -1,38 +1,37 @@
+"""A module containing operations concerning propositional logic."""
 from typing import Tuple
-import pprint
+
 OPERATOR_ARITY = {"&": 2, "|": 2, "¬": 1, ">": 2, "=": 2}
 
-class syntax_tree:
+
+class ASTNode:
+    """Represents a node of an abstract syntax tree."""
+
     def __init__(self, symbol: str, variable: bool = False) -> None:
         self.symbol = symbol
         self.variable = variable
         self.children = []
-    
-    def __repr__(self) -> str:
-        if self.children:
-            return f"{self.symbol}: {self.children}"
-        else:
-            return f"{self.symbol}"
 
-def parse_symbol(inp: str, pos: int) -> Tuple[syntax_tree, int]:
+    def __repr__(self) -> str:
+        return f"{self.symbol}: {self.children}" if self.children else self.symbol
+
+
+def parse_symbol(inp: str, pos: int) -> Tuple[ASTNode, int]:
+    """Parses the given string from the given position."""
     if inp[pos] in OPERATOR_ARITY:
-        top = syntax_tree(inp[pos])
+        top = ASTNode(inp[pos])
         curr = pos + 1
         for _ in range(OPERATOR_ARITY[inp[pos]]):
             temp = parse_symbol(inp, curr)
-            if isinstance(temp, tuple):
-                top.children.append(temp[0]); curr = temp[1]
-            else:
-                top.children.append(temp)
-        return tuple([top, curr])
-    else:
-        return syntax_tree(inp[pos], True)
-    
-def generate_binary(num: int):
-    res = []
-    p = 0
-    for _ in range(2 ** num):
-        p += 1
-        yield [(p >> i) for i in range(num)]
+            top.children.append(temp[0])
+            curr = temp[1]
+        return (top, curr)
+    return (ASTNode(inp[pos], True), pos + 1)
 
-pprint.pprint(parse_symbol("&|pq¬r", 0)[0])
+
+def generate_truth_table_row(num: int):
+    """Returns a generator that will produce a row of inputs for a truth table."""
+    count = 0
+    for _ in range(2 ** num):
+        count += 1
+        yield [(count >> i) for i in range(num)]
